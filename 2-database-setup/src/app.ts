7f95a7688673f2f4d3cc86419bcf7d1fc9b4bd84
= <=>
2019-10-30 09:07:40 +1100

    Add mongoose connection

--- 2-database-setup/src/app.ts
+++ 2-database-setup/src/app.ts
@@ -1,12 +1,17 @@
+// src/app/ts
 import { ApolloServer } from 'apollo-server'
 import typeDefs from './schema'
 import resolvers from './resolvers/index'
+import connect from './database/connect';
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers })
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }: { url: string }) => {
+server.listen().then(async ({ url }: { url: string }) => {
     console.log(`🚀  Server ready at ${url}`)
+
+    // Connect to your database
+    await connect({ db: 'mongodb://localhost:27017/test' })
 })
