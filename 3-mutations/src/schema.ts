7d418317d3ec55571dfb1d3762432d22cfeb9c76
Tom Nagle <tomanagle@gmail.com>
2019-10-31 19:38:50 +1100

    Add _id to card type

--- 3-mutations/src/schema.ts
+++ 3-mutations/src/schema.ts
@@ -1,22 +1,14 @@
 // src/schema.ts
 import { gql } from 'apollo-server'
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = gql`
-    # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-    # This "Card" type defines the queryable fields for every card in our data source.
     type Card {
+        _id: ID!
         title: String
         author: String
         body: String
     }
 
-    # The "Query" type is special: it lists all of the available queries that
-    # clients can execute, along with the return type for each. In this
-    # case, the "cards" query returns an array of zero or more Cards (defined above).
     type Query {
         cards: [Card]
     }
