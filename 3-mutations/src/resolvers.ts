2412278cc38459c41e8896e4d1b2d0601b97a395
= <=>
2019-10-31 16:40:38 +1100

    Add mutations

--- 3-mutations/src/resolvers.ts
+++ 3-mutations/src/resolvers.ts
@@ -1,9 +1,18 @@
 // src/resolvers/ts
-import { getAllCards, GetAllCardsInput } from './controllers/card.controller'
+import {
+    getAllCards,
+    GetAllCardsInput,
+    createCard,
+    CreateCardInput,
+} from './controllers/card.controller'
 const resolvers = {
     Query: {
         cards: (_: null, { input }: { input: GetAllCardsInput }) =>
             getAllCards({ ...input }),
+    },
+    Mutations: {
+        CreateCard: (_: null, { input }: { input: CreateCardInput }) =>
+            createCard({ ...input }),
     },
 }
 
